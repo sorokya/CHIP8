a543c92a6118a42dfed144fb586ae825e94ee75f
Richard Leek <richard@prositeone.com>
2016-11-07 18:48:13 -0500

    move chip8 to new file

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,7 @@
+mod chip8;
+use chip8::CHIP8;
+
 fn main() {
     let chip = CHIP8::new();
     println!("{:?}", chip);
-}
-
-const RAM_SIZE: usize = 4096;
-const GPR_COUNT: usize = 16;
-
-#[derive(Debug)]
-struct CHIP8 {
-    ram: Vec<u8>,
-
-    // Current opcode
-    opcode: u16,
-
-    // General Purpose Registers
-    v: Vec<u8>,
-
-    // Index Register
-    i: u16,
-
-    // Program Counter
-    pc: u16,
-}
-
-impl CHIP8 {
-    pub fn new() -> CHIP8 {
-        let ram = vec![0; RAM_SIZE];
-
-        CHIP8 {
-            ram: ram,
-            opcode: 0,
-            v: vec![0; GPR_COUNT],
-            i: 0,
-            pc: 0,
-        }
-    }
 }
