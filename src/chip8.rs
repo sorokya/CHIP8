bbd213fa972b34beb981600074dc4f45dd01401e
Richard Leek <richard@prositeone.com>
2016-11-07 23:00:13 -0500

    add sfml and create a window

--- src/chip8.rs
+++ src/chip8.rs
@@ -2,8 +2,8 @@ use std::io::Read;
 
 const RAM_SIZE: usize = 4096;
 const GPR_COUNT: usize = 16;
-const SCREEN_WIDTH: usize = 64;
-const SCREEN_HEIGHT: usize = 32;
+static SCREEN_WIDTH: usize = 64;
+static SCREEN_HEIGHT: usize = 32;
 const STACK_SIZE: usize = 16;
 const NUMBER_OF_KEYS: usize = 16;
 
@@ -30,9 +30,6 @@ const FONT_SET: [u8; 80] = [
 pub struct CHIP8 {
     ram: Vec<u8>,
 
-    // Current opcode
-    opcode: u16,
-
     // General Purpose Registers
     v: Vec<u8>,
 
@@ -58,7 +55,61 @@ pub struct CHIP8 {
 }
 
 impl CHIP8 {
-    pub fn tick(&self) {
+    pub fn tick(&mut self) {
+        let opcode = ((self.ram[self.pc as usize] as u16) << 8 | self.ram[self.pc as usize + 1] as u16);
+
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let kk = (opcode & 0x00FF) as u8;
+        let nnn = (opcode & 0x0FFF) as u16;
+        let n = (opcode & 0x000F) as u8;
+
+        match opcode & 0xF000 {
+            0x6000 => self.ld(kk, x),
+            0xA000 => self.ldi(nnn),
+            0xD000 => {}, // TODO draw something
+            0x2000 => self.call(nnn),
+            0xF000 => {
+                match opcode & & 0x00FF {
+                    0x0029 => self.ldfvx(x),
+                    0x0033 => self.ldbvx(x),
+                    0x0065 => self.ldvxi(x),
+                    _ => panic!("Unrecognized opcode {:X}", opcode),
+                }
+            },
+            _ => panic!("Unrecognized opcode {:X}", opcode),
+        }
+
+        self.pc += 2;
+    }
+
+    fn ld(&mut self, value: u8, register: usize) {
+        self.v[register] = value;
+    }
+
+    fn ldi(&mut self, value: u16) {
+        self.i = value;
+    }
+
+    fn call(&mut self, value: u16) {
+        self.stack.push(self.pc);
+        self.pc = value;
+    }
+
+    fn ldbvx(&mut self, register: usize) {
+        self.ram[self.i as usize] = self.v[register] / 100;
+        self.ram[(self.i + 1) as usize] = (self.v[register] / 10) % 10;
+        self.ram[(self.i + 2) as usize] = (self.v[register] % 100) % 10;
+    }
+
+    fn ldvxi(&mut self, end_register: usize) {
+        for i in 0..end_register {
+            self.ram[self.i as usize] = self.v[i];
+        }
+    }
+
+    fn ldfvx(&mut self, register: usize) {
+        self.i = FONT_SET[self.v[register] as usize] as u16;
     }
 
     pub fn done(&self) -> bool {
@@ -87,14 +138,13 @@ impl CHIP8 {
 
         CHIP8 {
             ram: ram,
-            opcode: 0,
             v: vec![0; GPR_COUNT],
             i: 0,
             pc: 0x200,
             gfx: vec![0; SCREEN_WIDTH * SCREEN_HEIGHT],
             delay_timer: 0,
             sound_timer: 0,
-            stack: vec![0; STACK_SIZE],
+            stack: Vec::new(),
             sp: 0,
             key: vec![0; NUMBER_OF_KEYS],
         }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::fs::File;
 use std::env;
 
+extern crate sfml;
+use sfml::graphics::{CircleShape, Color, ConvexShape, Font, RenderTarget, RenderWindow, Sprite,
+                     Text, Texture, Transformable};
+use sfml::window::{Key, VideoMode, event, window_style};
+use sfml::system::Vector2f;
+
+static SCREEN_WIDTH: u32 = 64;
+static SCREEN_HEIGHT: u32 = 32;
+static SCREEN_SCALE: u32 = 8;
+
 mod chip8;
 use chip8::CHIP8;
 
@@ -12,9 +22,27 @@ fn main() {
 
     println!("Loading ROM {}", rom_name);
 
-    let chip = CHIP8::new(&mut File::open(&rom_name).unwrap());
+    let mut chip = CHIP8::new(&mut File::open(&rom_name).unwrap());
+
+    let mut window = RenderWindow::new(VideoMode::new_init(SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE, 32),
+                                       "CHIP8",
+                                       window_style::CLOSE,
+                                       &Default::default())
+        .unwrap();
+    window.set_vertical_sync_enabled(true);
 
     while !chip.done() {
         chip.tick();
+
+        for event in window.events() {
+            match event {
+                event::Event::Closed |
+                event::Event::KeyPressed { code: Key::Escape, .. } => return,
+                _ => {}
+            }
+        }
+
+        window.clear(&Color::black());
+        window.display();
     }
 }
